3952e5c97fac034d631b35672cd915dd9d2daf82
Rafshanul Hoque Siam <rafshanulsiam811@gmail.com>
2021-07-21 00:10:05 +0600

    #6 fixes . recently view products done

--- src/components/product/productsRecentlyView/ProductsRecentlyView.jsx
+++ src/components/product/productsRecentlyView/ProductsRecentlyView.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import { Row, Col, Container } from 'react-bootstrap'
 import { recentlyViewProductDetails } from '../../../data'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleLeft, faAngleRight ,faTrophy } from '@fortawesome/free-solid-svg-icons'
+
 import './ProductsRecentlyView.css'
 const ProductsRecentlyView = () => {
     return <>
-        <Container style={{ border: "1px solid red" }} className="mt-5">
+        <Container className="mt-5">
             <Row>
+                <Col xs={12} md={12} sm={12} lg={12} className="mb-3" style={{ borderBottom: "1px solid #f0f0f0", }}>
+                    <button type="button" className="btn btn-warning " style={{ float: "left", padding: "1% 5%", color: "white", fontWeight: "bold", background: "#FED90F", border: "none" }}>
+                    <FontAwesomeIcon icon={faTrophy} />   RECENTLY VIEWED PRODUCTS
+                        </button>
+                    <div style={{ textAlign: "right" }}>
+                        <button type="button" type="button" class="btn btn-light m-1" > <FontAwesomeIcon icon={faAngleLeft} /></button>
+                        <button type="button" type="button" class="btn btn-light" ><FontAwesomeIcon icon={faAngleRight} /></button>
+                    </div>
+                </Col>
                 {recentlyViewProductDetails.map((e, i) => {
                     return <Col xs={6} md={4} sm={6} lg={4}>
                         <CustomCard value={e} />
@@ -29,7 +41,7 @@ const CustomCard = (props) => {
                 <div className="col-lg-6 col-md-5 col-sm-12 col-xs-12 ">
                     <h5 classNameName="card-title">{props.value && props.value.title}</h5>
                     {props.value.catList.map((e, i) => <span className=" customCat" style={{ color: "#98989C", fontWeight: "500" }} key={i} onMouseEnter={changeBackground} onMouseLeave={changeBackgroundonLeave}>
-                        {e}<br/>
+                        {e}<br />
                     </span>)}
                     <span classNameName="card-text" style={{ color: "#98989C", fontWeight: "500" }}>
                         more....
@@ -42,11 +54,11 @@ const CustomCard = (props) => {
 
 }
 
-const  changeBackground=(e)=> {
+const changeBackground = (e) => {
     e.target.style.color = '#FFED94';
     e.target.style.cursor = 'pointer';
 
-  }
-  const  changeBackgroundonLeave=(e)=> {
+}
+const changeBackgroundonLeave = (e) => {
     e.target.style.color = '#9898AC';
-  }
+}
