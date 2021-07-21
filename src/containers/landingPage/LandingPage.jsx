bec7956e308c2995625baedced51893dd8f3ca5d
Rafshanul Hoque Siam <rafshanulsiam811@gmail.com>
2021-07-21 19:19:30 +0600

    #11 fixes

--- src/containers/landingPage/LandingPage.jsx
+++ src/containers/landingPage/LandingPage.jsx
@@ -7,7 +7,8 @@ import ProductLatest from '../../components/product/productLatest/productLatest'
 import Blogs from '../../components/blogs/Blogs';
 import AddBanner from '../../components/addBanner/AddBanner';
 import Product from '../../components/product/product/Product'
-import {products} from '../../data'
+import { products } from '../../data'
+import BottomBanner from '../../assets/bottomBanner.PNG'
 
 const LandingPage = () => {
     return <Container>
@@ -29,14 +30,17 @@ const LandingPage = () => {
                 <AddBanner />
                 <Container>
                     <Row>
-                        {products.map((e,i)=>{
-                            return  <Col xs={12} md={6} sm={12} lg={3} >
-                            <Product product={e} serial={i} key={i}/>
-                        </Col>
+                        {products.map((e, i) => {
+                            return <Col xs={12} md={6} sm={12} lg={3} >
+                                <Product product={e} serial={i} key={i} />
+                            </Col>
                         })}
-                       
+
                     </Row>
                 </Container>
+            </Col>
+            <Col xs={12} md={12} sm={12} lg={12} align="center" className="mt-5">
+                <img src={BottomBanner} alt="sans" height="70%" width="98%" className="mt-5"/>
             </Col>
         </Row>
     </Container>
